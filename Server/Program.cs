295b6eb8064ecc5d4a0200dc3f417f97dfbf3b0d
Alexander Litvin <litvin_alexander@mail.ru>
2026-07-23 16:12:45 +0300

    ву

--- Server/Program.cs
+++ Server/Program.cs
@@ -159,8 +159,8 @@ using (var scope = app.Services.CreateScope())
     else
     {
         // На проде выполняем миграции, чтобы не потерять данные
-        //db.Database.Migrate();
-        db.Database.EnsureCreated();
+        db.Database.Migrate();
+        //db.Database.EnsureCreated();
     }
 }
 
